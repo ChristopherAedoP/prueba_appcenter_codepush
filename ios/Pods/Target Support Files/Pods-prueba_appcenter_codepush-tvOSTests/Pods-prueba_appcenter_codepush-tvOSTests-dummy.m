5eea4a9412729c0c3ff157a2ffbb1a237ac4fed0
Christopher Aedo <christopher.aedo.p@gmail.com>
2019-01-09 19:31:07 -0300

    primer commit

---
+++ ios/Pods/Target Support Files/Pods-prueba_appcenter_codepush-tvOSTests/Pods-prueba_appcenter_codepush-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_prueba_appcenter_codepush_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_prueba_appcenter_codepush_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-prueba_appcenter_codepushTests/Pods-prueba_appcenter_codepushTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_prueba_appcenter_codepushTests : NSObject
+@end
+@implementation PodsDummy_Pods_prueba_appcenter_codepushTests
+@end
